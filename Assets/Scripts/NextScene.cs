aaf42e85f3ff2dd2db5a0e8433a297907114ae93
Xavier Arias <xavier_rate@outlook.com>
2021-11-21 12:52:36 +1100

    Beginning SnowShadergraph!

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int buildIndex;
+    [SerializeField] private GameObject GO;
+    
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(GO);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SceneManager.LoadSceneAsync(buildIndex - 1);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SceneManager.LoadSceneAsync(buildIndex + 1);
+        }
     }
 }
