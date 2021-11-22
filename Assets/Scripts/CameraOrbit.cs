4f1a72a210797aaf498557a2f2f96b30094739bb
Xavier Arias <xavier_rate@outlook.com>
2021-11-22 22:09:56 +1100

    Created a Force Field and Fog Particle System!

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CameraOrbit : MonoBehaviour
+    {
+        public float speed;
+
+        private void Update()
+        {
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+    }
+}
